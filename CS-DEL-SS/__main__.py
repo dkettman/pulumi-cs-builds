2f4f8c71a08b5b2741b38617e1cbddb05ab043f2
Ubuntu <cs_admin@ansible-awx.szwvkcfej14e1icapdk5ajdffb.bx.internal.cloudapp.net>
2024-10-20 15:56:21 +0000

    Saving to make sure that I don't lose it all again!

--- CS-DEL-SS/__main__.py
+++ CS-DEL-SS/__main__.py
@@ -1,185 +1,328 @@
+import ipaddress
+import json
+
 import pulumi
 from pulumi_azure_native import resources, network, compute
-from pulumi_random import random_string
-import pulumi_tls as tls
-import base64
+from pulumi_azure_native.network import (
+    GetVirtualNetworkResult,
+    get_virtual_network_output,
+)
+
+from typing import List
 
 # Import the program's configuration settings
 config = pulumi.Config()
-vm_name = config.get("vmName", "my-server")
-vm_size = config.get("vmSize", "Standard_A1_v2")
-os_image = config.get("osImage", "Debian:debian-11:11:latest")
-admin_username = config.get("adminUsername", "pulumiuser")
-service_port = config.get("servicePort", "80")
+stack = pulumi.get_stack()
+svrs = json.loads(config.get("servers"))
+imgs = json.loads(config.get("images"))
+roles = json.loads(config.get("roles"))
 
-os_image_publisher, os_image_offer, os_image_sku, os_image_version = os_image.split(":")
+# Set some variable (may need to goto config)
+subnet_mask = 24
+vnet_name = "vnet-CyberSolve-Labs"
 
-# Create an SSH key
-ssh_key = tls.PrivateKey(
-    "ssh-key",
-    algorithm="RSA",
-    rsa_bits=4096,
+
+# for s in svrs:
+#     print(svrs[s]["vmName"])
+#     print("  Type:", s)
+#     print("  Extras:", roles[s]["extras"])
+#     print("  Image:", imgs[roles[s]["osImage"]]["sku"])
+
+# Locate the "rg-CyberSolve-Labs" resource group
+resource_group = resources.get_resource_group(resource_group_name="rg-CyberSolve-Labs")
+# pulumi.export("RG:", resource_group)
+
+# # Locate the "vnet-CyberSolve-Labs" virtual Network
+# vnet = network.get_virtual_network(
+#     "vNet-CyberSolve-Labs",
+#     resource_group_name=resource_group.name,
+#     virtual_network_name="vNet-CyberSolve-Labs",
+# )
+
+vnet: pulumi.Output[GetVirtualNetworkResult] = get_virtual_network_output(
+    resource_group_name=resource_group.name, virtual_network_name=vnet_name
 )
 
-# Create a resource group
-resource_group = resources.ResourceGroup("resource-group")
 
-# Create a virtual network
-virtual_network = network.VirtualNetwork(
-    "network",
-    resource_group_name=resource_group.name,
-    address_space={
-        "address_prefixes": [
-            "10.0.0.0/16",
-        ],
-    },
-    subnets=[
-        {
-            "name": f"{vm_name}-subnet",
-            "address_prefix": "10.0.1.0/24",
-        },
-    ],
-)
-# Use a random string to give the VM a unique DNS name
-domain_name_label = random_string.RandomString(
-    "domain-label",
-    length=8,
-    upper=False,
-    special=False,
-).result.apply(lambda result: f"{vm_name}-{result}")
+def get_next_subnet_prefix(vnet: GetVirtualNetworkResult) -> str:
+    if not vnet.address_space or not vnet.address_space.address_prefixes:
+        raise ValueError("No address spaces found in the current VNet")
 
-# Create a public IP address for the VM
-public_ip = network.PublicIPAddress(
-    "public-ip",
-    resource_group_name=resource_group.name,
-    public_ip_allocation_method=network.IpAllocationMethod.DYNAMIC,
-    dns_settings={
-        "domain_name_label": domain_name_label,
-    },
-)
+    vnet_address_space: ipaddress.IPv4Network | ipaddress.IPv6Network = (
+        ipaddress.ip_network(address=vnet.address_space.address_prefixes[0])
+    )
+    possible_subnets = list(vnet_address_space.subnets(new_prefix=subnet_mask))
 
-# Create a security group allowing inbound access over ports 80 (for HTTP) and 22 (for SSH)
-security_group = network.NetworkSecurityGroup(
-    "security-group",
-    resource_group_name=resource_group.name,
+    occupied_address_spaces: list[ipaddress.IPv4Network | ipaddress.IPv6Network] = (
+        [
+            ipaddress.ip_network(subnet.address_prefix)
+            for subnet in vnet.subnets
+            if subnet.address_prefix
+        ]
+        if vnet.subnets
+        else []
+    )
+
+    pulumi.log.info(f"Vnet address space: {vnet_address_space}")
+    pulumi.log.info(f"Occupied subnets: {occupied_address_spaces}")
+
+    for subnet in possible_subnets:
+        if not any(subnet.overlaps(occupied) for occupied in occupied_address_spaces):
+            pulumi.log.info(f"First available /{subnet_mask} subnet: {str(subnet)}")
+            return str(subnet)
+
+    raise ValueError(
+        "No available subnet prefixes found in the current VNet address space"
+    )
+
+
+new_subnet_prefix: pulumi.Output[str] = vnet.apply(get_next_subnet_prefix)
+
+# if network.get_subnet(
+#     resource_group_name=resource_group.name,
+#     subnet_name="CS-subnet-" + pulumi.get_stack(),
+#     virtual_network_name=vnet_name,
+# ):
+#     subnet = network.get_subnet(
+#         resource_group_name=resource_group.name,
+#         subnet_name="CS-subnet-" + pulumi.get_stack(),
+#         virtual_network_name=vnet_name,
+#     )
+# else:
+#     subnet = network.Subnet(
+#         address_prefix=new_subnet_prefix,
+#         resource_name="CS-DEL-subnet-" + pulumi.get_stack(),
+#         resource_group_name=resource_group.name,
+#         subnet_name="CS-DEL-subnet-" + pulumi.get_stack(),
+#         virtual_network_name=vnet_name,
+#     )
+
+subnet: network.Subnet
+
+try:
+    print("trying....")
+    subnet = network.get_subnet(
+        resource_group_name=resource_group.name,
+        subnet_name="CS-DEL-subnet-" + pulumi.get_stack(),
+        virtual_network_name=vnet_name,
+    )
+except:
+    print("Got to the except")
+    subnet = network.Subnet(
+        address_prefix=new_subnet_prefix,
+        resource_name="CS-DEL-subnet-" + pulumi.get_stack(),
+        resource_group_name=resource_group.name,
+        subnet_name="CS-DEL-subnet-" + pulumi.get_stack(),
+        virtual_network_name=vnet_name,
+    )
+
+
+network_security_group = network.NetworkSecurityGroup(
+    resource_name="CS-DEL-nsg-" + pulumi.get_stack(),
+    location=config.get("location", "EastUS2"),
+    resource_group_name="rg-CyberSolve-Labs",
+    network_security_group_name="CS-DEL-nsg-" + pulumi.get_stack(),
     security_rules=[
         {
-            "name": f"{vm_name}-securityrule",
-            "priority": 1000,
-            "direction": network.AccessRuleDirection.INBOUND,
-            "access": "Allow",
-            "protocol": "Tcp",
-            "source_port_range": "*",
-            "source_address_prefix": "*",
+            "access": network.SecurityRuleAccess.ALLOW,
             "destination_address_prefix": "*",
-            "destination_port_ranges": [
-                service_port,
-                "22",
-            ],
-        },
+            "destination_port_range": "3389",
+            "direction": network.SecurityRuleDirection.INBOUND,
+            "name": "RDP-Inbound",
+            "priority": 130,
+            "protocol": network.SecurityRuleProtocol.ASTERISK,
+            "source_address_prefix": "*",
+            "source_port_range": "*",
+        }
     ],
 )
 
-# Create a network interface with the virtual network, IP address, and security group
-network_interface = network.NetworkInterface(
-    "network-interface",
-    resource_group_name=resource_group.name,
-    network_security_group={
-        "id": security_group.id,
-    },
-    ip_configurations=[
-        {
-            "name": f"{vm_name}-ipconfiguration",
-            "private_ip_allocation_method": network.IpAllocationMethod.DYNAMIC,
-            "subnet": {
-                "id": virtual_network.subnets.apply(lambda subnets: subnets[0].id),
-            },
-            "public_ip_address": {
-                "id": public_ip.id,
-            },
-        },
-    ],
-)
 
-# Define a script to be run when the VM starts up
-init_script = f"""#!/bin/bash
-    echo '<!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <title>Hello, world!</title>
-    </head>
-    <body>
-        <h1>Hello, world! 👋</h1>
-        <p>Deployed with 💜 by <a href="https://pulumi.com/">Pulumi</a>.</p>
-    </body>
-    </html>' > index.html
-    sudo python3 -m http.server {service_port} &
-    """
+def build_computer_resource(
+    vm_name: str, vm_type: str, vm_extras: List[str], vm_image: dict
+) -> compute.VirtualMachineArgs:
 
-# Create the virtual machine
-vm = compute.VirtualMachine(
-    "vm",
-    resource_group_name=resource_group.name,
-    network_profile={
-        "network_interfaces": [
-            {
-                "id": network_interface.id,
-                "primary": True,
-            }
-        ]
-    },
-    hardware_profile={
-        "vm_size": vm_size,
-    },
-    os_profile={
-        "computer_name": vm_name,
-        "admin_username": admin_username,
-        "custom_data": base64.b64encode(bytes(init_script, "utf-8")).decode("utf-8"),
-        "linux_configuration": {
-            "disable_password_authentication": True,
-            "ssh": {
-                "public_keys": [
-                    {
-                        "key_data": ssh_key.public_key_openssh,
-                        "path": f"/home/{admin_username}/.ssh/authorized_keys",
-                    },
-                ],
-            },
-        },
-    },
-    storage_profile={
-        "os_disk": {
-            "name": f"{vm_name}-osdisk",
-            "create_option": compute.DiskCreateOption.FROM_IMAGE,
-        },
-        "image_reference": {
-            "publisher": os_image_publisher,
-            "offer": os_image_offer,
-            "sku": os_image_sku,
-            "version": os_image_version,
-        },
-    },
-)
+    pip_config = None
 
-# Once the machine is created, fetch its IP address and DNS hostname
-vm_address = vm.id.apply(
-    lambda id: network.get_public_ip_address_output(
+    # Check for Extras
+    if "PIP" in vm_extras:
+        print("Configuring PIP!")
+        pip_config = {
+            "name": "CS-DEL-DC-pip-" + vm_name + pulumi.get_stack(),
+            "public_ip_allocation_method": "Static",
+        }
+
+    pass
+
+    # # Build the NIC
+    nic = network.NetworkInterface(
+        resource_name="CS-DEL-nic-" + vm_name + "-" + pulumi.get_stack(),
         resource_group_name=resource_group.name,
-        public_ip_address_name=public_ip.name,
+        network_interface_name="CS-DEL-nic-" + vm_name + "-" + pulumi.get_stack(),
+        ip_configurations=[
+            {
+                "name": "CS-DEL-DC-ipconfig-" + pulumi.get_stack(),
+                "primary": True,
+                "public_ip_allocation_configuration": pip_config,
+                "subnet": network.SubnetArgs(id=subnet.id),
+            }
+        ],
     )
+
+    # Build the Virtual Machine Object from assembled parts
+    obj = compute.VirtualMachineArgs(
+        resource_group_name=resource_group.name,
+        hardware_profile=compute.HardwareProfileArgs(vm_size=roles[vm_type]["vmSize"]),
+        network_profile=compute.NetworkProfileArgs(
+            network_interfaces=[
+                compute.NetworkInterfaceReferenceArgs(id=nic.id, primary=True),
+            ],
+        ),
+        os_profile=compute.OSProfileArgs(
+            admin_password=config.get("adminPassword"),
+            admin_username=config.get("adminUsername"),
+            computer_name=vm_name,
+        ),
+        storage_profile={
+            "image_reference": imgs[roles[vm_type]["osImage"]],
+            "os_disk": {
+                "caching": compute.CachingTypes.READ_WRITE,
+                "create_option": compute.DiskCreateOptionTypes.FROM_IMAGE,
+                "delete_option": "Delete",
+                "managed_disk": {
+                    "storage_account_type": compute.StorageAccountTypes.STANDARD_LRS,
+                },
+                "name": "CS-DEL-DC-osDisk-" + vm_name + "-" + pulumi.get_stack(),
+            },
+        },
+        vm_name="CS-DEL-" + vm_type + "-" + pulumi.get_stack(),
+    )
+    new_obj = compute.VirtualMachine(
+        resource_name="CS-DEL-" + vm_type + "-" + pulumi.get_stack(),
+        args=obj,
+    )
+    pass
+
+
+build_computer_resource(
+    "foo", "DC", ["PIP", "AD", "CS", "ADTools", "SQL", "RMQ"], "foo"
 )
 
-# Export the VM's hostname, public IP address, HTTP URL, and SSH private key
-pulumi.export("ip", vm_address.ip_address)
-pulumi.export("hostname", vm_address.dns_settings.apply(lambda settings: settings.fqdn))
-pulumi.export(
-    "url",
-    vm_address.dns_settings.apply(
-        lambda settings: f"http://{settings.fqdn}:{service_port}"
-    ),
-)
-pulumi.export(
-    "privatekey",
-    ssh_key.private_key_openssh,
-)
+# for s in svrs:
+#     pulumi.log.info(f'Starting Server: {svrs[s]["vmName"]}')
+#     svrs[s]["res"] = compute.VirtualMachine
+
+# vm_dc = compute.VirtualMachine(
+#     resource_name="CS-DEL-DC-" + pulumi.get_stack(),
+#     hardware_profile={"vm_size": compute.VirtualMachineSizeTypes.STANDARD_B2S},
+#     location=config.get("location", "EastUS"),
+#     network_profile={
+#         "network_api_version": compute.NetworkApiVersion.NETWORK_API_VERSION_2020_11_01,
+#         "network_interface_configurations": [
+#             {
+#                 "ip_configurations": [
+#                     {
+#                         "name": "CS-DEL-DC-ipconfig-" + pulumi.get_stack(),
+#                         "primary": True,
+#                         "public_ip_address_configuration": {
+#                             "name": "CS-DEL-DC-pip-" + pulumi.get_stack(),
+#                             "public_ip_allocation_method": "Static",
+#                         },
+#                         "subnet": network.SubnetArgs(id=subnet.id),
+#                     },
+#                 ],
+#                 "name": "CS-DEL-DC-nic-" + pulumi.get_stack(),
+#                 "primary": True,
+#             }
+#         ],
+#     },
+#     os_profile={
+#         "admin_password": "Cyb3rS0lve!!",
+#         "admin_username": "cs_admin",
+#         "computer_name": "CS-DEL-DC-" + pulumi.get_stack(),
+#     },
+#     resource_group_name=resource_group.name,
+#     storage_profile={
+#         "image_reference": images["W22c"],
+#         "os_disk": {
+#             "caching": compute.CachingTypes.READ_WRITE,
+#             "create_option": compute.DiskCreateOptionTypes.FROM_IMAGE,
+#             "managed_disk": {
+#                 "storage_account_type": compute.StorageAccountTypes.STANDARD_LRS,
+#             },
+#             "name": "CS-DEL-DC-osDisk-" + pulumi.get_stack(),
+#         },
+#     },
+#     vm_name="CS-DEL-DC-" + pulumi.get_stack(),
+# )
+# ##################################################################################################
+
+# # cyber_solve__pam = compute.VirtualMachine("CyberSolve-PAM",
+# #     additional_capabilities={
+# #         "hibernation_enabled": False,
+# #     },
+# #     diagnostics_profile={
+# #         "boot_diagnostics": {
+# #             "enabled": True,
+# #         },
+# #     },
+# #     hardware_profile={
+# #         "vm_size": azure_native.compute.VirtualMachineSizeTypes.STANDARD_B2S,
+# #     },
+# #     location="eastus",
+# #     network_profile={
+# #         "network_interfaces": [{
+# #             "delete_option": azure_native.compute.DeleteOptions.DELETE,
+# #             "id": "/subscriptions/c10368aa-7348-4c86-b047-33e52b5bd6c5/resourceGroups/CyberSolve-PAM/providers/Microsoft.Network/networkInterfaces/del-ss-dc497",
+# #         }],
+# #     },
+# #     os_profile={
+# #         "admin_username": "cs_admin",
+# #         "allow_extension_operations": True,
+# #         "computer_name": "DEL-SS-DC",
+# #         "require_guest_provision_signal": True,
+# #         "windows_configuration": {
+# #             "enable_automatic_updates": True,
+# #             "enable_vm_agent_platform_updates": False,
+# #             "patch_settings": {
+# #                 "assessment_mode": azure_native.compute.WindowsPatchAssessmentMode.IMAGE_DEFAULT,
+# #                 "enable_hotpatching": False,
+# #                 "patch_mode": azure_native.compute.WindowsVMGuestPatchMode.AUTOMATIC_BY_OS,
+# #             },
+# #             "provision_vm_agent": True,
+# #         },
+# #     },
+# #     resource_group_name="CYBERSOLVE-PAM",
+# #     security_profile={
+# #         "security_type": azure_native.compute.SecurityTypes.TRUSTED_LAUNCH,
+# #         "uefi_settings": {
+# #             "secure_boot_enabled": True,
+# #             "v_tpm_enabled": True,
+# #         },
+# #     },
+# #     storage_profile={
+# #         "disk_controller_type": azure_native.compute.DiskControllerTypes.SCSI,
+# #         "image_reference": {
+# #             "offer": "WindowsServer",
+# #             "publisher": "MicrosoftWindowsServer",
+# #             "sku": "2019-datacenter-gensecond",
+# #             "version": "latest",
+# #         },
+# #         "os_disk": {
+# #             "caching": azure_native.compute.CachingTypes.READ_WRITE,
+# #             "create_option": azure_native.compute.DiskCreateOptionTypes.FROM_IMAGE,
+# #             "delete_option": azure_native.compute.DiskDeleteOptionTypes.DELETE,
+# #             "disk_size_gb": 127,
+# #             "managed_disk": {
+# #                 "id": "/subscriptions/c10368aa-7348-4c86-b047-33e52b5bd6c5/resourceGroups/CYBERSOLVE-PAM/providers/Microsoft.Compute/disks/DEL-SS-DC_OsDisk_1_da6bb611fed84405a7946c7687576f78",
+# #                 "storage_account_type": azure_native.compute.StorageAccountTypes.STANDARD_SS_D_LRS,
+# #             },
+# #             "name": "DEL-SS-DC_OsDisk_1_da6bb611fed84405a7946c7687576f78",
+# #             "os_type": azure_native.compute.OperatingSystemTypes.WINDOWS,
+# #         },
+# #     },
+# #     tags={
+# #         "server_type": "dc",
+# #     },
+# #     vm_name="DEL-SS-DC",
+# #     opts = pulumi.ResourceOptions(protect=True))
